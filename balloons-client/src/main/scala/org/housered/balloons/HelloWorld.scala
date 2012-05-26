ffde3ace0573e03ebc256c1232ba3f7e6166c9fd
ecresswell <ecresswell@gmail.com>
2012-05-26 21:28:43 +0100

    Jme3 works now. Java doesn't though.

---
+++ balloons-client/src/main/scala/org/housered/balloons/HelloWorld.scala
@@ -0,0 +1,13 @@
+package org.housered.balloons
+import com.jme3.app.SimpleApplication
+
+object HelloWorld extends SimpleApplication {
+
+  def main(args: Array[String]) {
+    start()
+  }
+
+  def simpleInitApp() {
+
+  }
+}
---
+++ balloons-client/src/main/scala/org/housered/balloons/JavaWorld.java
@@ -0,0 +1,9 @@
+package org.housered.balloons;
+
+public class JavaWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("ARSE");
+    }
+}
